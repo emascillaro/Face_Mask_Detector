8d7a484af041fe1a236571d4beda39df0265e01c
emascillaro <emascillaro@gmail.com>
2020-09-12 16:59:11 -0400

    test2

---
+++ test2.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import zipfile
+import os
+
+
+# Download zip file
+!wget --no-check-certificate \
+  https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip \
+  -O /tmp/cats_and_dogs_filtered.zip
+
+# Extract zip file in tmp folder
+local_zip = '/tmp/cats_and_dogs_filtered.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('tmp')
+zip_ref.close()
+
+base_dir = 'tmp/cats_and_dogs_filtered'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat/dog pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat/dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+print('total training cat images :', len(os.listdir(      train_cats_dir ) ))
+print('total training dog images :', len(os.listdir(      train_dogs_dir ) ))
+
+print('total validation cat images :', len(os.listdir( validation_cats_dir ) ))
+print('total validation dog images :', len(os.listdir( validation_dogs_dir ) ))
+
+train_cat_fnames = os.listdir( train_cats_dir )
+train_dog_fnames = os.listdir( train_dogs_dir )
+
+print(train_cat_fnames[:10])
+print(train_dog_fnames[:10])
